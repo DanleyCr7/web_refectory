ecb5f87770bc07d1c72c5f65ad7d235f6a3a636b
DanrleyCR7 <danrleysil46@gmail.com>
2021-08-15 08:38:23 -0300

    sicronizando com a api

--- src/pages/warning/index.js
+++ src/pages/warning/index.js
@@ -1,12 +1,15 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Badge from '@material-ui/core/Badge';
 import WarningTable from '../../components/warningTable';
+import api from '../../services/api';
 const Warning = _ => {
     const [requested, requestedSet]= useState([
     ])
     const [accepted, acceptedSet]= useState([
         {teacher: 'Danrley', warning: 'Os alunos estão com fome', class: 'Informatica', quantidy: 40, bout: 'Manhã', year:'2', date: '24/07/2021' }
     ]) 
+
+   
     return (
         <main style={{ flexGrow: 1 }}>
             <div style={{
